c237471414c52ca5ed5e185956a5819cbb8cd90a
andriibeha <beha.andrii@gmail.com>
2022-05-20 21:57:30 +0300

    Add Erorr

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 import './css/styles.css';
+import { fetchCountries } from "./server/fetchCountries.js";
+
+
+console.log(fetchCountries('jap')).then(data => { 
+    console.log(data.length)
+})
+
+
+
 
 const DEBOUNCE_DELAY = 300;
 
-console.log("HI")
+
